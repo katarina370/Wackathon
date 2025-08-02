32880625d84f41c647b49b80b3a730b59f2c459f
Jedh <jedh.racho@gmail.com>
2025-08-02 13:14:44 +1200

    main and login route, with login basic css

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# route for main/landing page
 @app.route('/')
 def home():
     return render_template('index.html')  # serves main page
 
+# route for login page
 @app.route('/login')
 def login():
-    return render_template('login.html')  # you can create this later
+    # login stuff here
+    return render_template('login.html')
 
+# route for map page
 @app.route('/map')
 def map_page():
-    return render_template('map.html')  # you can plug in your current map page
+    return render_template('map_interface.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
