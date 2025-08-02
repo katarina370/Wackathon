6053b2e810b2c2641e688d5f14afe7566aa0d86e
Jedh <jedh.racho@gmail.com>
2025-08-02 12:46:08 +1200

    App

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Homepage<h1>'
+    return render_template('index.html')  # serves main page
+
+@app.route('/login')
+def login():
+    return render_template('login.html')  # you can create this later
+
+@app.route('/map')
+def map_page():
+    return render_template('map.html')  # you can plug in your current map page
 
 if __name__ == '__main__':
     app.run(debug=True)
